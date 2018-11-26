8534affe52ee18530a567ae562a3f6a80a4f323b
Benjamin Darcet <b.darcet@gmail.com>
2018-11-26 17:23:40 +0100

    player actions(7)

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,7 +2,6 @@ class Player < ApplicationRecord
   belongs_to :user
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
-  validates :photo, presence: true
   validates :level, presence: true
   validates :price_per_day, presence: true
   validates :position, presence: true
