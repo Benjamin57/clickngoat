50475ff1cf0c6da383a34a5c42988d037bc54db8
Maxime Jeantet <maximejeantet12@gmail.com>
2018-11-29 15:17:50 +0100

    Front to back calendar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_28_093710) do
+ActiveRecord::Schema.define(version: 2018_11_29_140517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2018_11_28_093710) do
     t.bigint "player_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "pick_your_date"
+    t.date "end_date"
     t.index ["player_id"], name: "index_bookings_on_player_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
