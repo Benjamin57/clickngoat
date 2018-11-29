641d6bace74e55a1bb4a335302a66b55b388e11c
Maxime Jeantet <maximejeantet12@gmail.com>
2018-11-29 12:27:51 +0100

    front calendar booking

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@ import "bootstrap";
 import { toggleTab } from 'components/tab';
 import 'select2/dist/css/select2.css';
 import { initSelect2 } from '../components/init_select2';
+import "../plugins/flatpickr";
 
 
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
